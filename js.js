009f1ba695b5a50a98ab328550a6142e15fdde64
batoi-1271 <batoi1271.doe@gmail.com>
2021-05-09 22:14:32 +0700

    upload cac file

---
+++ js.js
@@ -0,0 +1,13 @@
+var user = document.getElementById("username");
+var pass = document.getElementById("pass");
+var login = document.getElementById("login");
+var tk = "toi";
+var mk = "123";
+login.onclick = function(){
+    if(user.value == tk && pass.value== mk){
+        window.open("main.html");
+    }
+    else{
+        alert("tai khoan mat khau sai");
+    }
+}
